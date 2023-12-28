19a7807e88ba437c6b248d106914b57627f2ec3e
oujiayi <kelly.ou@mail.utoronto.ca>
2023-12-28 03:20:38 -0500

    added stars animation.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,20 @@
-import './index.scss';
-import Sidebar from '../Sidebar';
-import { Outlet } from 'react-router-dom';
+import './index.scss'
+import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return (
-        <div className="App"> 
-          <Sidebar/>
-          <div className='page'>
-            <Outlet/>
-          </div>
-        </div>
-    ) 
+  return (
+    <div className="App ">
+      <Sidebar />
+      <div className="page">
+        <Outlet />
+      </div>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+    </div>
+  )
 }
 
 export default Layout
