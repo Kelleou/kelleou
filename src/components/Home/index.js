ce1ee75133ac25e91aa30a346be4d73d0f63982b
oujiayi <kelly.ou@mail.utoronto.ca>
2023-12-27 23:31:08 -0500

    home screen styling.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,15 +2,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
-import Kel from "./Kel";
 const Home = () => {
     const [letterClass] = useState('text-animate')
     const nameArray = "Hi, I am Kelly".split('')
 
-
     return (
-        <div className="container home-page">
-            <div className="text-zone">
+        <div className="container md:mx-auto home-page flex h-screen">
+            <div className="grid grid-cols-6 m-auto">
+                <span></span>
+                <span className="col-span-5">
+                <span className="inline-block align-baseline ..."> </span>
                 <h1> 
                     <AnimatedLetters letterClass={letterClass}
                     strArray={nameArray}
@@ -19,6 +20,7 @@ const Home = () => {
                     ! </h1>
                 <h2>A Third Year Computer Science Student at University of Toronto</h2>
                 <Link to="about" className="flat-button"> MORE ABOUT ME</Link>
+                </span>
             </div>
         </div>
     );
