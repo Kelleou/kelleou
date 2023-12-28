8ea39f9f97626f0fdb84c4b6a3ed5379e8527ca9
oujiayi <kelly.ou@mail.utoronto.ca>
2023-12-28 00:39:29 -0500

    about me page style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,60 +1,66 @@
 import './index.scss'
-import AnimatedLetters from "../AnimatedLetters"
-import {useState} from "react";
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 import kel2 from '../../assets/images/kel2.PNG'
 
 const About = () => {
-    const [letterClass] = useState('text-animate')
+  const [letterClass] = useState('text-animate')
 
-    return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={"ABOUT ME".split('')}
-                    idx={1}
-                    /> 
-                </h1>
-                <p>
-                    I am a second-year computer science student at University of Toronto.
-                    I am currently completing a CS Specialist program with intended focuses in web technologies and artificial intelligence.
-                </p>
-                <p>
-                    Besides coding, I also love to draw! &#40; I drew all the little characters on this website : &#41;
-                    <br/>
-                    I also love food. I love cooking new recipes and eating out.
-                </p>
-            </div>
+  return (
+    <div className="about-page grid md:grid-cols-7 gap-20">
+      <span></span>
+      <span className="col-span-3">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'ABOUT ME'.split('')}
+            idx={1}
+          />
+        </h1>
+        <p>
+          I am a third year computer science student at University of Toronto
+          who is passionate about solving real-world problems and committed to
+          continuous learning. I am actively seeking summer 2024 internship
+          opportunities to apply my skills and contribute innovative solutions
+          to the tech industry! Apart from coding, I also love to paint and
+          draw. I have interest in UI design and hope to blend art andn
+          technology to create compelling and user-focused interfaces!
+        </p>
+      </span>
 
-            <div className='text-zone-1'>
-                <h2>
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={"SKILLS & COURSES".split('')}
-                    idx={1}
-                    /> 
-                    <br/>
-                </h2>
-                <p1>
-                    <strong>Projects: </strong> Click the github icon on the left to see all my projects!
-                    <br/>
-                    <strong>Programming Languages: </strong> Java, Python, JavaScript, HTML, R, MIPS assembly 
-                    <br/>
-                    Currently learning: C, C++
-                    <br/>
-                    <strong>IDEs:</strong> Visual Studio Code, IntelliJ, PyCharm
-                    <br/>
-                    <strong>Tools:</strong> React.js, Node.js, Google Firebase, Git
-                    <br/>
-                    <strong>Courses:</strong> <br/>
-                    First Year Courses: &#40;CSC110, CSC111 &#41;  <br/>
-                    Software Design &#40;CSC207&#41; <br/>
-                    Theory of Computation &#40;CSC236&#41; <br/>
-                    Computer Organization &#40;CSC258&#41;
-                </p1>
-            </div>
-            <img className="kel2-container" alt="kel" />
-        </div>
-    )
+      <span className="col-span-3">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'SKILLS & COURSES'.split('')}
+            idx={1}
+          />
+        </h1>
+        <p>
+          Programming Languages: Java, Python, C, SQL, JavaScript, HTML, CSS
+          <br />
+          IDEs: Visual Studio Code, IntelliJ, PyCharm
+          <br />
+          Tools & Framework: React.js, Angular, Node.js, Firebase, Git, GitHub
+          <br />
+          <details>
+            <summary>Courses</summary>
+            <p>
+              Software Design (CSC207) <br />
+              Theory of Computation (CSC236) <br />
+              Computer Organization (CSC258) <br />
+              Software Tools and Systems Programming (CSC209) <br />
+              Programming on the Web (CSC309) <br />
+              Introduction to Software Engineering (CSC301) <br />
+              Algorithm Design, Analysis & Complexity (CSC373) <br />
+              Introduction to Databases (CSC343)
+              <br />
+            </p>
+          </details>
+        </p>
+      </span>
+    </div>
+  )
 }
 
 export default About
